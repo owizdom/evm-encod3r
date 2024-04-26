2589dd5bbe9de8c10e7c0255f2152cd3732bd195
ramenboy <154702154+ramenboy88@users.noreply.github.com>
2024-04-26 17:20:30 +0100

    Add files via upload

---
+++ evmr3v3rs3r.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract Encode {
+    function encode(string memory _string1, uint _uint, string memory _string2) public pure returns (bytes memory) {
+        return (abi.encode(_string1, _uint, _string2));
+    }
+    function decode(bytes memory data) public pure returns (string memory _str1, uint _number, string memory _str2) {
+        (_str1, _number, _str2) = abi.decode(data, (string, uint, string));            
+    }
+}
